781e11841e890a3b172b8db7ded8372d00860d23
nphamvn <pvnam95@hotmail.com>
2022-02-25 15:40:10 +0900

    from-example4(string)

--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
 import { from } from 'rxjs';
 
-//Observable from collection
-const map = new Map();
-map.set(1, 'One');
-map.set(2, 'Two');
+//Observable from string
+const souce = from('Hello World');
 
-const souce = from(map);
-
-const subscribe = souce.subscribe((val) => console.log(map.get(val[0])));
+const subscribe = souce.subscribe((val) => console.log(val));
